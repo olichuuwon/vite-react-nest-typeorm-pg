550914cf365ee0acac3fa18d6104a3ebe07e979a
jieshilyn <39185220+olichuuwon@users.noreply.github.com>
2025-11-21 14:45:28 +0800

    chore: rename server folder to backend

---
+++ my-app/backend/src/attendance/attendance.module.ts
@@ -0,0 +1,11 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AttendanceRecord } from "./attendance.entity";
+
+@Module({
+  imports: [TypeOrmModule.forFeature([AttendanceRecord])],
+  controllers: [],
+  providers: [],
+  exports: [TypeOrmModule],
+})
+export class AttendanceModule {}
